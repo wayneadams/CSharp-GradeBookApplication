382329e0c3fb11ce63a6a9b2db18bda8be81b780
wayneadams <wadams19@gmail.com>
2018-03-19 18:02:34 -0700

    Implements GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,6 +11,7 @@ namespace GradeBook.GradeBooks
 {
     using GradeBook.Enums;
     using System;
+    using System.Linq;
 
     /// <summary>
     /// The ranked grade book.
@@ -35,6 +36,25 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Ranked Grading requires a minimum of 5 students to work");
             }
 
+            var orderedStudents = this.Students.OrderByDescending(s => s.AverageGrade);
+            var gradebucketSize = Students.Count / 5;
+
+            if(averageGrade >= orderedStudents.ElementAt(gradebucketSize - 1).AverageGrade)
+            {
+                return 'A';
+            } 
+            if (averageGrade >= orderedStudents.ElementAt((gradebucketSize * 2) - 1).AverageGrade)
+            {
+                return 'B';
+            }
+            if (averageGrade >= orderedStudents.ElementAt((gradebucketSize * 3) - 1).AverageGrade)
+            {
+                return 'C';
+            }
+            if (averageGrade >= orderedStudents.ElementAt((gradebucketSize * 4) - 1).AverageGrade)
+            {
+                return 'D';
+            }
             return 'F';
         }
     }
